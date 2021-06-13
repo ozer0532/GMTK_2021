8749e2c98d874093ebe833e867cc6829f711371b
ozer0532 <ozer0532@gmail.com>
2021-06-13 15:38:16 +0700

    Added sfx to attack

--- Assets/Scripts/Settings/MusicButton.cs
+++ Assets/Scripts/Settings/MusicButton.cs
@@ -2,15 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MusicButton : MonoBehaviour
 {
     public AudioMixer mixer;
+    public Sprite onSprite;
+    public Sprite offSprite;
+    public Image image;
 
     private bool isOn = true;
 
     public void Toggle()
     {
+        print(isOn);
         isOn = !isOn;
         if (isOn) On();
         else Off();
@@ -20,11 +25,13 @@ public class MusicButton : MonoBehaviour
     {
         mixer.SetFloat("Music/Volume", 0f);
         mixer.SetFloat("SFX/Volume", 0f);
+        image.sprite = onSprite;
     }
 
     public void Off()
     {
         mixer.SetFloat("Music/Volume", -100f);
         mixer.SetFloat("SFX/Volume", -100f);
+        image.sprite = offSprite;
     }
 }
